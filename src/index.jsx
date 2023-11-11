81b9aa01165da48e009e264c5469f9b610118f5a
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-11 08:33:35 +0100

    React router completed

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 import { App } from './components/App';
+import { HomePage } from './pages/HomePage';
+import { ReservationPage } from './pages/ReservationPage';
 import './global.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/reservation',
+        element: <ReservationPage />,
+      },
+    ],
   },
 ]);
 
 createRoot(document.querySelector('#app')).render(
-  <RouterProvider router={router} />
+  <RouterProvider router={router} />,
 );
